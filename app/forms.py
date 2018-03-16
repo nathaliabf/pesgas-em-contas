b603e0197d119ad272de54d472e20c518b0f5220
GiovanaSachett <giovanasachett@gmail.com>
2018-03-16 20:28:37 -0300

    commit login users

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember_me = BooleanField('Remamber Me')
 	submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Usuario', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Senha', validators=[DataRequired()])
+	password2 = PasswordField('Garantir que não errou:', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Registrar')
+	
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user is not None:
+			raise ValidationError('Escolha um nome de usuario diferente.')
+			
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is not None:
+			raise ValidationError('Esse email ja esta em uso.')		
